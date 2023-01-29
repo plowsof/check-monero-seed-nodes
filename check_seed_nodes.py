179cc51ec02514f8f4400d634d66d344a3637005
plowsof <plowsof@protonmail.com>
2023-01-29 03:18:35 +0000

    tor/ipv4 basic

---
+++ check_seed_nodes.py
@@ -0,0 +1,73 @@
+import sys
+import socket
+import socks
+import os
+
+from levin.section import Section
+from levin.bucket import Bucket
+from levin.ctypes import *
+from levin.constants import P2P_COMMANDS, LEVIN_SIGNATURE
+
+testnet_id= b'\x12\x30\xF1\x71\x61\x04\x41\x61\x17\x31\x00\x82\x16\xA1\xA1\x11'
+stagenet_id= b'\x12\x30\xF1\x71\x61\x04\x41\x61\x17\x31\x00\x82\x16\xA1\xA1\x12'
+#os.chdir("./py_levin")
+def check_ip(host,ip):
+    global testnet_id,stagenet_id
+    network = None
+    if str(ip).startswith("3"):
+        network = stagenet_id
+    if str(ip).startswith("2"):
+        network = testnet_id
+    try:
+        socks.setdefaultproxy(socks.PROXY_TYPE_SOCKS5, "127.0.0.1", 9050, True)
+        sock = socks.socksocket()
+        #sock = socket.socket()
+        sock.settimeout(30)
+        sock.connect((host,ip))
+    except:
+        sys.stderr.write("unable to connect to %s:%d\n" % (host, ip))
+        return False
+
+    bucket = Bucket.create_handshake_request(network_id=network)
+
+    sock.send(bucket.header())
+    sock.send(bucket.payload())
+
+    # print(">> sent packet \'%s\'" % P2P_COMMANDS[bucket.command])
+    buckets = []
+    while 1:
+        buffer = sock.recv(8)
+        if not buffer:
+            sys.stderr.write("Invalid response; exiting\n")
+            return False
+
+        if not buffer.startswith(bytes(LEVIN_SIGNATURE)):
+            sys.stderr.write("Invalid response; exiting\n")
+            return False
+
+        return True
+
+with open("net_node.inl","r") as f:
+    lines = f.readlines()
+
+at_ipv4=0
+at_anon=0
+nodes = []
+for line in lines:
+    if "::get_ip_seed_nodes()" in line:
+        at_ipv4 = 1
+    if "full_addrs.insert" in line and at_ipv4 == 1:
+            nodes.append(line.split("\"")[1])
+    if "return full_addrs;" in line and at_ipv4 == 1:
+        at_ipv4 = 0
+    if "onion" in line:
+        nodes.append(line.split("\"")[1])
+
+for node in nodes:
+    host = node.split(":")[0]
+    ip=node.split(":")[1]
+    if check_ip(host, int(ip)):
+        print(f"{node} online")
+    else:
+        print(f"{node} offline")
+
--- readme.md
+++ readme.md
@@ -1,1 +1,28 @@
-hello world
+todo: emojis and daily github workflow
+
+
+```
+176.9.0.187:28080 online
+88.99.173.38:28080 online
+51.79.173.165:28080 online
+192.99.8.110:28080 online
+37.187.74.171:28080 online
+176.9.0.187:38080 online
+88.99.173.38:38080 online
+51.79.173.165:38080 online
+192.99.8.110:38080 online
+37.187.74.171:38080 online
+176.9.0.187:18080 online
+88.198.163.90:18080 online
+66.85.74.134:18080 online
+88.99.173.38:18080 online
+51.79.173.165:18080 online
+192.99.8.110:18080 online
+37.187.74.171:18080 online
+xwvz3ekocr3dkyxfkmgm2hvbpzx2ysqmaxgter7znnqrhoicygkfswid.onion:18083 online
+4pixvbejrvihnkxmduo2agsnmc3rrulrqc7s3cbwwrep6h6hrzsibeqd.onion:18083 online
+zbjkbsxc5munw3qusl7j2hpcmikhqocdf4pqhnhtpzw5nt5jrmofptid.onion:18083 online
+qz43zul2x56jexzoqgkx2trzwcfnr6l3hbtfcfx54g4r3eahy3bssjyd.onion:18083 online
+plowsof3t5hogddwabaeiyrno25efmzfxyro2vligremt7sxpsclfaid.onion:18083 online
+plowsoffjexmxalw73tkjmf422gq6575fc7vicuu4javzn2ynnte6tyd.onion:18083 online
+```
